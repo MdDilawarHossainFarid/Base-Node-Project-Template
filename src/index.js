5ea490ffbd24c85f93577e0f7224a386a2887905
Md Dilawar Hossain Farid <47987656+MdDilawarHossainFarid@users.noreply.github.com>
2023-08-18 16:52:18 +0530

    basic project setup

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const { ServerConfig, Logger } = require("./config");
+
+const apiRoutes = require("./routes");
+
+const app = express();
+
+app.use("/api", apiRoutes);
+
+app.listen(ServerConfig.PORT, () => {
+  console.log(
+    `Successfully started the server  on PORT : ${ServerConfig.PORT}`
+  );
+  // Logger.info("Successfullu started the server", "root", {});
+});
